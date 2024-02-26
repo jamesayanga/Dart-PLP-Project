342a413efba939543400cbbc15a3d2a1932ebb01
jamesayanga <ayangajames@gmail.com>
2024-02-26 17:51:40 +0300

    first dart project

---
+++ main.dart
@@ -0,0 +1,16 @@
+//my first dart code
+void  main() { 
+  String school  = "PLP AFRICA ACADEMY";
+  int total = 400;
+  double GPA = 4.0;
+  num year = 2024;
+  bool isMarried = true;
+
+  //lets print our variable
+  print("Hello World! My First Dart Class");
+  print (school);
+  print(total);
+  print(GPA);
+  print(year);
+  print (isMarried);
+}
